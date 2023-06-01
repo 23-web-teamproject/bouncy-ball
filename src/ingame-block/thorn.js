bc859b5e345c50c233f034b68085041bf9bf18c5
10cheon00 <10cheon00@gmail.com>
2023-06-02 00:00:13 +0900

    fix: thorn extends Sprite

--- src/ingame-block/thorn.js
+++ src/ingame-block/thorn.js
@@ -1,6 +1,6 @@
 import { Vector, Sprite, Rect, GameObject, Color } from "/src/engine/module.js";
 
-export default class Thorn extends GameObject {
+export default class Thorn extends Sprite {
   /**
    * 밟으면 더 높이 뛰어오르는 블록입니다.
    *
@@ -8,24 +8,7 @@ export default class Thorn extends GameObject {
    * @param {number} y - y좌표
    */
   constructor(x, y) {
-    super();
-
-    this.addChild(new Rect({
-      name: "thorn",
-      width: 28,
-      height: 1,
-      color: new Color(256, 256, 256, 1),
-      transform: {
-        position: new Vector(x, y - 14.5),
-      },
-      isPhysicsEnable: true,
-      rigidbody: {
-        isStatic: true,
-        isTrigger: true,
-      },
-    }));
-
-    this.addChild(new Sprite({
+    super({
       name: "thornimg",
       imagePath: "/src/ingame-block/thorn.png",
       isPhysicsEnable: true,
@@ -35,6 +18,21 @@ export default class Thorn extends GameObject {
       rigidbody: {
         isStatic: true,
       },
-    }));
+    });
+
+    const trigger = new Rect({
+      name: "thorn",
+      width: 26,
+      height: 1,
+      transform: {
+        position: new Vector(0, -14.5),
+      },
+      isPhysicsEnable: true,
+      rigidbody: {
+        isTrigger: true,
+      },
+    })
+    this.addChild(trigger);
+    trigger.hide();
   }
 }
